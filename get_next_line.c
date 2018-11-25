815ba8c8be3c9150433a9b69e58c3859e6a850fb
Emmanuel ZONDA <ezonda@e1r8p7.42.fr>
2018-11-25 12:31:52 +0100

    blablbla

--- get_next_line.c
+++ get_next_line.c
@@ -6,7 +6,7 @@
 /*   By: ezonda <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/11/25 08:53:07 by ezonda            #+#    #+#             */
-/*   Updated: 2018/11/25 11:51:01 by ezonda           ###   ########.fr       */
+/*   Updated: 2018/11/25 12:31:27 by ezonda           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,13 +16,10 @@
 #include <sys/types.h>
 #include <fcntl.h>
 
-int		ft_check(int fd, char **str, char **line)
+int		ft_check(int fd, char **line)
 {
 	if (line == NULL || fd < 0)
 		return (-1);
-	if (!str)
-		if(!(*str = ft_strnew(1)))
-			return (-1);
 	return (0);
 }
 
@@ -36,7 +33,9 @@ int		ft_fill_line(char *str, char **line)
 		return (0);
 	if (!str)
 		return (-1);
-	else
+//	printf("2\n");
+//	getchar();
+	if (str)
 	{
 		if (ft_strchr(&str[i], '\n'))
 		{
@@ -44,6 +43,8 @@ int		ft_fill_line(char *str, char **line)
 				i++;
 			if (str[i] == '\n')
 			{
+//				printf("3\n");
+//				getchar();
 				*line = ft_strsub(str, start, i - start);
 				i++;
 				return (1);
@@ -51,6 +52,8 @@ int		ft_fill_line(char *str, char **line)
 		}
 		else
 		{
+//			printf("4\n");
+//			getchar();
 			*line = ft_strdup(&str[i]);
 			i = -1;
 			if (!ft_strlen(*line))
@@ -68,9 +71,13 @@ int		get_next_line(int fd, char **line)
 	char *tmp;
 	int ret;
 
-	if (ft_check(fd, &str, line) == -1)
+//	printf("start\n");
+//	getchar();
+	if (ft_check(fd, line) == -1)
 		return(-1);
-	str = ft_strnew(1);
+	if (!str)
+		if (!(str = ft_strnew(1)))
+			return (-1);
 	while ((ret = read(fd, buffer, BUFF_SIZE)))
 	{
 		if (ret < 0)
@@ -82,6 +89,7 @@ int		get_next_line(int fd, char **line)
 		if (ft_strchr(buffer, '\n'))
 			break;
 	}
-	ft_fill_line(str, &(*line));
-	return (1);
+//	printf("1\n");
+//	getchar();
+	return (ft_fill_line(str, line));
 }
